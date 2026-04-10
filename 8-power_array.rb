c502baf9164c338f011d7cc5ae6358c80880055c
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-10 18:00:15 +0530

    added suggested changes

--- 8-power_array.rb
+++ 8-power_array.rb
@@ -1,19 +1,12 @@
-if ARGV.empty? || ARGV[0].strip.empty?
+if ARGV.empty? || ARGV.length < 2
   puts "Please provide an input"
   exit
 end
 
 class Array
-  def group_by_length
-    result = Hash.new { |h, k| h[k] = [] }
-
-    each do |element|
-      key = element.to_s.length
-      result[key] << element.to_s
-    end
-
-    result.sort.to_h
+  def power(x)
+    map { |num| num ** x }
   end
 end
 
-puts eval(ARGV[0]).group_by_length.inspect
+puts eval(ARGV[0]).power(ARGV[1].to_i).inspect
