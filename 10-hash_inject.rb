7b76c562f016a0a012a5c7233bad8bcb360d4436
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-10 18:11:43 +0530

    added suggested changes

--- 10-hash_inject.rb
+++ 10-hash_inject.rb
@@ -1,27 +1,30 @@
-if ARGV.length == 0 || ARGV[0].strip.empty?
+if ARGV.empty? || ARGV[0].strip.empty?
   puts "Please provide an input"
   exit
 end
 
-arr = eval(ARGV[0])
+class Array
+  def group_by_length
+    result = Hash.new { |h, k| h[k] = [] }
 
-grouped = {}
+    each do |element|
+      key = element.to_s.length
+      result[key] << element.to_s
+    end
 
-arr.each do |elem|
-  str = elem.to_s
-  len = str.length
+    result.sort.to_h
+  end
 
-  grouped[len] ||= []
-  grouped[len] << str
+  def group_by_parity
+    group_by_length.inject({ odd: [], even: [] }) do |acc, (key, value)|
+      if key.odd?
+        acc[:odd] << value
+      else
+        acc[:even] << value
+      end
+      acc
+    end
+  end
 end
 
-result = grouped.sort.inject({}) do |acc, (len, values)|
-  key = len.even? ? "even" : "odd"
-
-  acc[key] ||= []
-  acc[key] << values
-
-  acc
-end
-
-puts result
+puts eval(ARGV[0]).group_by_parity.inspect
