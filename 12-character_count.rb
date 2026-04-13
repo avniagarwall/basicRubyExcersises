9b1212498535fe18a09140abcee57ef92d07d0b1
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-13 10:10:52 +0530

    added suggested changes

--- 12-character_count.rb
+++ 12-character_count.rb
@@ -1,28 +1,37 @@
-if ARGV.length == 0 || ARGV[0].strip.empty?
+if ARGV.empty? || ARGV[0].strip.empty?
   puts "Please provide an input"
   exit
 end
 
-str = ARGV[0]
+class StringAnalyzer
+  LOWERCASE = /[a-z]/
+  UPPERCASE = /[A-Z]/
+  DIGIT     = /[0-9]/
 
-lower = 0
-upper = 0
-digit = 0
-special = 0
+  def initialize(str)
+    @str = str
+  end
 
-str.each_char do |ch|
-  if ('a'..'z').include?(ch)
-    lower += 1
-  elsif ('A'..'Z').include?(ch)
-    upper += 1
-  elsif ('0'..'9').include?(ch)
-    digit += 1
-  else
-    special += 1
+  def analyze
+    Hash.new(0).tap do |count|
+      @str.each_char do |ch|
+        case ch
+        when LOWERCASE then count[:lower]   += 1
+        when UPPERCASE then count[:upper]   += 1
+        when DIGIT     then count[:digit]   += 1
+        else                count[:special] += 1
+        end
+      end
+    end
+  end
+
+  def to_s
+    result = analyze
+    "Lowercase characters = #{result[:lower]}\n"  \
+    "Uppercase characters = #{result[:upper]}\n"  \
+    "Numeric characters   = #{result[:digit]}\n"  \
+    "Special characters   = #{result[:special]}"
   end
 end
 
-puts "Lowercase characters = #{lower}"
-puts "Uppercase characters = #{upper}"
-puts "Numeric characters = #{digit}"
-puts "Special characters = #{special}"
+puts StringAnalyzer.new(ARGV[0]).to_s
