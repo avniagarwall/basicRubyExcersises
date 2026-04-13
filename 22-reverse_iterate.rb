5ef43a8cb7b0121d2c6f4ae694635344343e0ff3
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-13 11:17:57 +0530

    added suggested changes

--- 22-reverse_iterate.rb
+++ 22-reverse_iterate.rb
@@ -1,12 +1,11 @@
-if ARGV.length == 0 || ARGV[0].strip.empty?
+if ARGV.empty? || ARGV[0].strip.empty?
   puts "Please provide an input"
   exit
 end
 
 class Array
   def reverse_iterate
-    i = self.length - 1
-
+    i = length - 1
     while i >= 0
       yield self[i]
       i -= 1
@@ -14,10 +13,4 @@ class Array
   end
 end
 
-arr = eval(ARGV[0])
-
-arr.reverse_iterate do |i|
-  print "#{i} "
-end
-
-puts
+puts eval(ARGV[0]).reverse_iterate { |i| print "#{i} " }
