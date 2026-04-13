0ef692e3eca161b1c4a03d8767dcd4864865f829
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-13 11:20:00 +0530

    added suggested changes

--- 23-sales_tax.rb
+++ 23-sales_tax.rb
@@ -1,61 +1,92 @@
 class Item
-  attr_accessor :name, :imported, :exempted, :price
+  SALES_TAX_RATE  = 0.10
+  IMPORT_DUTY_RATE = 0.05
 
-  def initialize(name, imported, exempted, price)
-    @name = name
+  attr_reader :name, :price, :imported, :exempted
+
+  def initialize(name:, price:, imported:, exempted:)
+    @name     = name
+    @price    = Float(price)
     @imported = imported
     @exempted = exempted
-    @price = price
   end
 
   def tax
-    tax = 0
-
-    # 10% basic tax if not exempted
-    tax += 0.10 * @price unless @exempted
-
-    # 5% import duty
-    tax += 0.05 * @price if @imported
-
+    tax = 0.0
+    tax += @price * SALES_TAX_RATE  unless @exempted
+    tax += @price * IMPORT_DUTY_RATE if @imported
     tax
   end
 
-  def final_price
+  def total_price
     @price + tax
+  end
+
+  def to_s
+    "#{@name}#{' (imported)' if @imported}: #{format('%.2f', total_price)} (tax: #{format('%.2f', tax)})"
   end
 end
 
-items = []
+class ShoppingCart
+  def initialize
+    @items = []
+  end
 
-loop do
-  print "Name of the product: "
-  name = gets.chomp
+  def add(item)
+    @items << item
+  end
 
-  print "Imported? (yes/no): "
-  imported = gets.chomp.downcase == "yes"
+  def grand_total
+    @items.sum(&:total_price).round
+  end
 
-  print "Exempted from sales tax? (yes/no): "
-  exempted = gets.chomp.downcase == "yes"
+  def total_tax
+    @items.sum(&:tax)
+  end
 
-  print "Price: "
-  price = gets.chomp.to_f
-
-  items << Item.new(name, imported, exempted, price)
-
-  print "Do you want to add more items to your list(y/n): "
-  break if gets.chomp.downcase == "n"
+  def display
+    puts "\n--- Receipt ---"
+    @items.each { |item| puts item }
+    puts "---------------"
+    puts "Total Tax   : #{format('%.2f', total_tax)}"
+    puts "Grand Total : #{grand_total}"
+  end
 end
 
-puts "\n----- BILL -----"
+class InputCollector
+  def self.yes?(prompt)
+    print prompt
+    gets.chomp.strip.downcase == 'yes'
+  end
 
-total = 0
+  def self.ask(prompt)
+    print prompt
+    gets.chomp.strip
+  end
 
-items.each do |item|
-  item_total = item.final_price
-  total += item_total
+  def self.collect
+    cart = ShoppingCart.new
 
-  puts "#{item.name}: #{item_total.round(2)}"
+    loop do
+      name     = ask("Name of the product: ")
+      imported = yes?("Imported? (yes/no): ")
+      exempted = yes?("Exempted from sales tax? (yes/no): ")
+      price    = ask("Price: ")
+
+      cart.add(Item.new(name: name, price: price, imported: imported, exempted: exempted))
+
+      break unless ask("Do you want to add more items to your list(y/n): ").downcase == 'y'
+    end
+
+    cart
+  end
 end
 
-puts "----------------"
-puts "Grand Total: #{total.round}"
+begin
+  cart = InputCollector.collect
+  cart.display
+rescue Interrupt
+  puts "\nExiting..."
+rescue => e
+  puts "Error: #{e.message}"
+end
