31e8cd51f00c1944264fdf75605c82d6b6a72e5f
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-13 10:58:37 +0530

    added suggested changes

--- 18-interest_difference.rb
+++ 18-interest_difference.rb
@@ -4,30 +4,35 @@ if ARGV.length < 2
 end
 
 class Interest
-  attr_accessor :p, :t
+  RATE = 0.10
 
-  def initialize
-    @r = 0.10   
-    yield self if block_given?
+  attr_reader :p, :t
+
+  def initialize(p, t)
+    @p = Float(p)
+    @t = Float(t)
+  end
+
+  def simple_interest
+    @p * (1 + RATE * @t)
+  end
+
+  def compound_interest
+    @p * (1 + RATE) ** @t
   end
 
   def difference
-    si = @p * (1 + @r * @t)
+    format("%.2f", compound_interest - simple_interest)
+  end
 
-    ci = @p * (1 + @r) ** @t
-
-    diff = ci - si
-
-    format("%.2f", diff)
+  def to_s
+    "Principal        : #{@p}\n"           \
+    "Time             : #{@t} years\n"     \
+    "Rate             : #{RATE * 100}% pa\n" \
+    "Simple Interest  : #{format('%.2f', simple_interest)}\n"   \
+    "Compound Interest: #{format('%.2f', compound_interest)}\n" \
+    "Difference       : #{difference}"
   end
 end
 
-p = ARGV[0].to_f
-t = ARGV[1].to_f
-
-obj = Interest.new do |i|
-  i.p = p
-  i.t = t
-end
-
-puts "Interest difference= #{obj.difference}"
+puts Interest.new(ARGV[0], ARGV[1]).to_s
