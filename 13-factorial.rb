7deb348e2c88335c8c098ac7d2748f1d50590135
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-13 10:14:01 +0530

    added suggested changes

--- 13-factorial.rb
+++ 13-factorial.rb
@@ -1,10 +1,16 @@
-if ARGV.length == 0 || ARGV[0].strip.empty?
+if ARGV.empty? || ARGV[0].strip.empty?
   puts "Please provide an input"
   exit
 end
 
-n = ARGV[0].to_i
+class Factorial
+  def initialize(n)
+    @n = Integer(n)
+  end
 
-result = (1..n).inject(1) { |fact, num| fact * num }
+  def compute
+    (1..@n).inject(1) { |fact, num| fact * num }
+  end
+end
 
-puts result
+puts Factorial.new(ARGV[0]).compute
