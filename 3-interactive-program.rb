95fa47ec631637e4bef94249a43bd256819f7e98
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-22 11:53:10 +0530

    commit 1

---
+++ 3-interactive-program.rb
@@ -0,0 +1,48 @@
+class InteractiveProgram
+  def initialize
+    @binding = binding
+    @buffer  = []
+  end
+
+  def run
+    puts "Interactive Ruby Program"
+    puts "Enter a blank line to evaluate."
+    puts "Enter 'q' to quit."
+    puts "-" * 40
+
+    loop do
+      print ">> "
+      line = gets.chomp
+
+      case line
+      when "q"
+        puts "Goodbye!"
+        break
+
+      when ""
+        evaluate
+
+      else
+        @buffer << line
+      end
+    end
+  end
+
+  private
+
+  def evaluate
+    return if @buffer.empty?
+
+    code = @buffer.join("\n")
+    @buffer.clear
+
+    begin
+      result = @binding.eval(code)
+      puts "=> #{result.inspect}"
+    rescue StandardError => e
+      puts "Error: #{e.message}"
+    end
+  end
+end
+
+InteractiveProgram.new.run
