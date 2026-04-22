6e0336c0a7da75277a2f460ceca0132f8c69529f
Avni Agarwal <avni111agarwal@gmail.com>
2026-04-22 11:34:28 +0530

    commit 1

---
+++ 2-dynamic-class.rb
@@ -0,0 +1,37 @@
+class DynamicClass
+  def initialize(class_name)
+    @class_name = class_name
+    @klass = Object.const_set(class_name, Class.new)
+  end
+
+  def def_method(method_name, method_body)
+    @klass.class_eval do
+      define_method(method_name) do
+        eval(method_body)
+      end
+    end
+  end
+
+  def call(method_name)
+    instance = @klass.new
+    instance.public_send(method_name)
+  end
+end
+
+
+print "Please enter the class name: "
+class_name = gets.chomp
+
+my_class = DynamicClass.new(class_name)
+
+print "Please enter the method name you wish to define: "
+method_name = gets.chomp
+
+print "Please enter the method's code: "
+method_body = gets.chomp
+
+my_class.def_method(method_name, method_body)
+
+puts "\n--- Result ---"
+puts "Hello, Your class #{class_name} with method #{method_name} is ready. Calling: #{class_name}.new.#{method_name}:"
+puts my_class.call(method_name).inspect
